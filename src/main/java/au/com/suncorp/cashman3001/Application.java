757c6c5a051eafdb0e7c777acfce551b48007b87
Luke <lukejdavis@hotmail.com>
2018-12-19 13:16:05 +1000

    asfsd

--- src/main/java/au/com/suncorp/cashman3001/Application.java
+++ src/main/java/au/com/suncorp/cashman3001/Application.java
@@ -1,9 +1,21 @@
 package au.com.suncorp.cashman3001;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.algolia.search.APIClient;
+import com.algolia.search.ApacheAPIClientBuilder;
+import com.algolia.search.Index;
+
+import au.com.suncorp.cashman3001.model.Contact;
 
 /**
  * Spring Boot Application
@@ -16,6 +28,11 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public RestTemplate getRestTemplate() {
+       return new RestTemplate();
     }
 
 }
